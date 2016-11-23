5e2188383166612110eb86c382c821853a9cd9da
Hayden Chudy <hayden.chudy@croscon.com>
2016-11-23 18:20:18 -0500

    New update_environment fixture for quick environment changes in tests.
    It's autoused, just provide the proper marker (`environ`).

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,40 @@
     :copyright: (c) 2016 by Croscon Consulting, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details
 """
+
+import os
+
+import pytest
+
+from fleaker._compat import text_type
+
+
+absent = object()
+
+@pytest.fixture(autouse=True)
+def update_environment(request):
+    """Update the environment based on what is provided in marks and then tear
+    it all down.
+    
+    Provide your environment overrides in the ``environ`` marker as a kwarg.
+    """
+    updates = request.node.get_marker('environ')
+    originals = {}
+
+    if not updates:
+        yield
+        return
+
+    updates = updates.kwargs
+
+    for key, val in updates.items():
+        originals[key] = os.environ.get(key, absent)
+        os.environ[key] = text_type(val)
+
+    yield
+
+    for key, val in originals.items():
+        if val is absent:
+            os.environ.pop(key, None)
+        else:
+            os.environ[key] = val
